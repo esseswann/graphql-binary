767f3529ce73542115d6dffbc938ca354a873f12
jsus <esseswann@gmail.com>
2020-02-14 21:39:53 +0300

    Response stub

---
+++ response.js
@@ -0,0 +1,14 @@
+export const encoder = (
+  query,
+  response,
+  schema
+) => Uint8Array()
+
+export const decoder = (
+  query,
+  response,
+  schema
+) => {
+  const data = {}
+  return { data }
+}
