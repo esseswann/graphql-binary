7f6c5a4c834b40dfea1449843a7ecd8b3af3a463
jsus <esseswann@gmail.com>
2020-04-19 18:34:07 +0300

    Separated decodeResponseField

--- src/response/decode.js
+++ src/response/decode.js
@@ -11,29 +11,26 @@ const decodeResponse = (
   const result = {}
   let offset = 0
   forEach(
-    ({ name }) => {
-      const metadata = find({ name: name.value, isArg: false }, dictionary.Query.decode)
-      if (metadata.kind !== 'Scalar')
-        return // FIXME
-      if (!metadata)
-        throw new Error(`Field ${name.value} was not found in the dictionary`)
-      if (!metadata.typeHandler)
-        throw new Error(`No handler for ${name.value}: ${metadata.type}`)
-      if (!metadata.typeHandler.decode)
-        throw new Error(`No decoder for ${name.value}: ${metadata.type}`)
-
-      const [value, nextOffset] = metadata.typeHandler.decode(offset, response)
-      result[name.value] = value
-      offset = nextOffset
-    },
+    ({ name }) => decodeResponseField(name.value, dictionary.Query.decode, offset, response, result),
     query.definitions[0].selectionSet.selections)
   console.log(result)
   return decode(response)
 }
 
-const decodePrimitive = (type, offset, data) =>
-  !types[type]
-    ? new Error(`Unknown decode type: ${type}`)
-    : types[type](offset, data)
+const decodeResponseField = (name, dictionary, offset, data, result) => {
+  const metadata = find({ name, isArg: false }, dictionary)
+  if (!metadata)
+    throw new Error(`Field ${name.value} was not found in the dictionary`)
+  if (metadata.kind !== 'SCALAR')
+    return // FIXME
+  if (!metadata.typeHandler)
+    throw new Error(`No handler for ${name.value}: ${metadata.type}`)
+  if (!metadata.typeHandler.decode)
+    throw new Error(`No decoder for ${name.value}: ${metadata.type}`)
+
+  const [value, nextOffset] = metadata.typeHandler.decode(offset, data)
+  result[name] = value
+  offset = nextOffset
+}
 
 export default decodeResponse
