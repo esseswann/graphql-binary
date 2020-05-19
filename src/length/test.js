dd5d89354c57e9ac9cb3046df78e731f89b44cb2
jsus <esseswann@gmail.com>
2020-05-19 15:41:39 +0300

    Length header with tests

---
+++ src/length/test.js
@@ -0,0 +1,13 @@
+import { encodeLength, decodeLength } from './'
+
+const lengths = [
+  2 ** 7 - 1,
+  2 ** 14 - 1,
+  2 ** 21 - 1,
+  2 ** 28 - 1,
+]
+
+lengths.map((value, index) => 
+  test(`correct ${index + 1} byte length`, () =>
+    expect(decodeLength(encodeLength(value)))
+      .toEqual([value, index + 1])))
