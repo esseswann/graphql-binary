368a585ee05f0df71e89faf1551dfbe2bb30d920
jsus <esseswann@gmail.com>
2020-07-25 12:09:54 +0300

    Testing different query types

--- src/query/test.js
+++ src/query/test.js
@@ -4,12 +4,16 @@ import { print } from 'graphql/language/printer'
 
 import generateDictionary from 'dictionary'
 import query from 'fixtures/basicQuery.graphql'
+import mutation from 'fixtures/mutation.graphql'
+import subscription from 'fixtures/subscription.graphql'
 import schema from 'fixtures/schema.graphql'
 
 import decode from './decode'
 import encode from './encode'
 
 delete query.loc // We do not store the source data
+delete mutation.loc // We do not store the source data
+delete subscription.loc // We do not store the source data
 
 const generatedDictonary = generateDictionary(buildSchema(schema))
 
@@ -24,6 +28,18 @@ test('decoded query matches encoded', () =>
     .then(dictionary =>
       expect(decode(encode(query, dictionary), dictionary))
         .toEqual(query)))
+
+test('mutation type is encoded', () =>
+  generatedDictonary
+    .then(dictionary => 
+      expect(decode(encode(mutation, dictionary), dictionary))
+        .toEqual(mutation)))
+
+test('subscription type is encoded', () =>
+  generatedDictonary
+    .then(dictionary => 
+      expect(decode(encode(subscription, dictionary), dictionary))
+        .toEqual(subscription)))
 
 test('binary representation at least twice smaller than string representation', () =>
   generatedDictonary
