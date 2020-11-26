278cbbe3db3500d473c01f1a5f30dea037e6e28d
jsus <esseswann@gmail.com>
2020-11-26 17:24:31 +0300

    Initial abstarctDecode refactoring

--- src/abstractTreeDecoder.js
+++ src/abstractTreeDecoder.js
@@ -6,6 +6,7 @@ const SCALAR = 0b00
 const VECTOR = 0b01
 const SCALAR_LIST = 0b10
 const VECTOR_LIST = 0b11
+const LIST = 0b10
 
 const objectHandler = (target) => keyHandler(target)
 
@@ -38,13 +39,51 @@ const keyHandler = curry((target, key, kind) => {
 const decodeInputObject = (
 ) => {
   const result = {}
-  sexyDecode(objectHandler(result), [0, 4, 1, 0, 3, 2, 3, 4, 5, 4, 1, 0, 7, END, 3, 3, 0, 8, END, 0, 9, 1, 0, 10, END, END, 0, 11, END])
+  decode(objectHandler(result), [0, 10, 1, 0, 11, END, END]) //2, 3, 4, 5, 4, 1, 0, 7, END, 3, 3, 0, 8, END, 0, 9, 1, 0, 10, END, END, 0, 11, END, END, END])
   console.log(result)
 }
 
+const decode = (
+  handler,
+  data,
+  index = 0
+) => {
+  const current = data[index]
+  const nextIndex = index + 1
+
+  if (current === END)
+    return nextIndex
+  else {
+    const { kind, name } = dict[current]
+    const nextHandler = handler(name, kind)
+
+    return decode(
+      handler,
+      data,
+      kind === SCALAR
+        ? prepareScalar(nextHandler, data, nextIndex)
+        : decode(nextHandler, data, nextIndex)
+    )
+  }
+}
+
+const prepareScalar = (handler, data, index) => {
+  // Read header
+  handler(data[index])
+  return index + 1
+}
+
+const handleList = (handler, data, index) => {
+  const jindex = data[index]
+  while (jindex > 0)
+    index = SCALAR & kind
+      ? prepareScalar(handler, data, index)
+      : decode(handler(), data, index)
+  return index
+}
 
 const sexyDecode = (handler, data, index = 0) => {
-  if (data[index] === END || index >= data.length) {
+  if (data[index] === END) {
     index += 1
     return index
   }
@@ -56,7 +95,6 @@ const sexyDecode = (handler, data, index = 0) => {
     handler(name, SCALAR)(data[index])
     index += 1
   } else if (kind === VECTOR) {
-    // Should return index here for purity
     index = sexyDecode(handler(name, VECTOR), data, index)
   } else if (kind === SCALAR_LIST) {
     let jindex = data[index]
@@ -72,7 +110,6 @@ const sexyDecode = (handler, data, index = 0) => {
     const nextHandler = handler(name, VECTOR_LIST)
     index += 1
     while (jindex > 0) {
-      // Should return index here for purity
       index = sexyDecode(nextHandler(), data, index)
       jindex -= 1
     }
