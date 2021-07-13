d74dcef213b981ef44c55b62c5911bfe17250bfc
jsus <esseswann@gmail.com>
2021-07-13 20:37:56 +0300

    Initial typescript tests

---
+++ src/query/test.ts
@@ -0,0 +1,13 @@
+import { buildSchema } from 'graphql'
+import compress from 'graphql-query-compress'
+import { print } from 'graphql/language/printer'
+
+// import generateDictionary from 'dictionary'
+// import query from '../fixtures/basicQuery.graphql'
+// import mutation from '../fixtures/mutation.graphql'
+// import schema from '../fixtures/schema.graphql'
+// import subscription from '../fixtures/subscription.graphql'
+// import queryWithVariables from '../fixtures/variables/query.graphql'
+// import variablesSchema from '../fixtures/variables/schema.graphql'
+
+test('encodes without errors', () => expect(1).toBe(1))
