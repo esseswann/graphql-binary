d6eafedf82493588131a448ef572338428fd48ca
jsus <esseswann@gmail.com>
2021-11-14 22:23:11 +0300

    More abstract varint target

--- src/varint/index.ts
+++ src/varint/index.ts
@@ -8,28 +8,28 @@ const MAX_SHIFTS = Math.floor(MAX_BITS / BYTE_SIZE)
 
 // This is LEB128 implementation
 export function decodeVarInt(data: ByteIterator): number {
-  let length: number = 0
+  let number: number = 0
   let index: number = 0
   let byte: number
 
   while (index < MAX_SHIFTS) { // Prevents infinite shifting attack
     byte = data.take()
-    length += (byte & 0x7f) << (index * 7)
+    number += (byte & 0x7f) << (index * 7)
     if (byte >> 7 === 0) break
     else index++
   }
 
-  return length
+  return number
 }
 
-export const encodeVarInt = (length: number): Uint8Array => {
+export const encodeVarInt = (number: number): Uint8Array => {
   const array: Array<number> = []
   let lowerBits: number
   let highBit: number
-  while (length > 0) {
-    lowerBits = length & 0x7f
-    length = length >> 7
-    highBit = length ? 1 : 0
+  while (number > 0) {
+    lowerBits = number & 0x7f
+    number = number >> 7
+    highBit = number ? 1 : 0
     array.push((highBit << 7) + lowerBits)
   }
 
