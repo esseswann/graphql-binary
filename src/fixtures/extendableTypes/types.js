d1a20658d79bc80f0f171eb68a50650d1139f6bf
jsus <esseswann@gmail.com>
2021-11-15 22:51:43 +0300

    Working response transcoding

--- src/fixtures/extendableTypes/types.js
+++ src/fixtures/extendableTypes/types.js
@@ -5,23 +5,23 @@ export const definitions = {
   // It is also nicely decoded directly to Date() object
   UDateS: {
     astName: 'IntValue',
-    encode: data => {
+    encode: (data) => {
       const timestampSeconds = data.getTime() / 1000
       return new Uint8Array([
         (timestampSeconds & 0xff000000) >> 24,
         (timestampSeconds & 0x00ff0000) >> 16,
         (timestampSeconds & 0x0000ff00) >> 8,
-        timestampSeconds & 0x000000ff,
+        timestampSeconds & 0x000000ff
       ])
     },
     decode: (offset, data) => {
       const view = new DataView(data.buffer)
       return [new Date(view.getInt32(offset) * 1000), offset + 4]
-    },
-  },
+    }
+  }
 }
 
 export const data = {
-   // Note that we set seconds to zero or tests will fail because we loose precision
-  date: new Date('December 17, 1995 03:24:00') 
+  // Note that we set seconds to zero or tests will fail because we loose precision
+  date: new Date('December 17, 1995 03:24:00')
 }
--- src/response/test.ts
+++ src/response/test.ts
@@ -1,7 +1,9 @@
+import { addMocksToSchema } from '@graphql-tools/mock'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import fs from 'fs'
-import { buildSchema } from 'graphql'
+import { buildSchema, graphql, print } from 'graphql'
 import {
-  BasicDocument, BasicQuery
+  BasicDocument
 } from '../fixtures'
 import Decoder from '../query/decode'
 import Encoder from '../query/encode'
@@ -14,35 +16,24 @@ const schema = buildSchema(schemaString)
 const decoder = new Decoder(schema)
 const encoder = new Encoder(schema)
 
-const data: BasicQuery = {
-  int: 10,
-  float: 5.5,
-  boolean: true,
-  string: 'string',
-  withArgs: 'withArgs',
-  scalarArray: ['scalar'],
-  mapArray: [{ 
-    id: 'id',
-    map: {
-      id: 'id',
-      map: {
-        id: 'id'
-      }
-    }
-  }],
-  map: {
-    id: 'id',
-    map: {
-      id: 'id',
-      map: {
-        id: 'id'
-      }
-    }
+const executableSchema = makeExecutableSchema({ typeDefs: schema })
+const schemaWithMocks = addMocksToSchema({
+  schema: executableSchema,
+  mocks: {
+    Int: () => Math.floor(1000 * Math.random()), // FIXME prevent negative integers for now
+    Float: () => 3.5, // FIXME should do something with precision being lost on decodes
   }
-}
-
-test('decoded response matches encoded', () => {
-  const encodedResponse = encode(decoder, BasicDocument, data)
-  const decodedResponse = decode(encoder, BasicDocument, encodedResponse)
-  expect(decodedResponse).toEqual(data)
 })
+
+test('decoded response matches encoded', async () => {
+  const response = await graphql({ schema: schemaWithMocks , source: print(BasicDocument) })
+  const encodedResponse = encode(decoder, BasicDocument, response.data)
+  const decodedResponse = decode(encoder, BasicDocument, encodedResponse)
+  expect(decodedResponse).toEqual(response.data)
+})
+
+// test('extendable types are applied', () =>
+//   generateDictionary(buildSchema(extendableTypesSchema), extendableTypes.definitions)
+//     .then(dictionary =>
+//         expect(decodeResponse(extendableTypesQuery, dictionary, encodeResponse(extendableTypesQuery, dictionary, extendableTypes.data)))
+//           .toEqual(extendableTypes.data)))
