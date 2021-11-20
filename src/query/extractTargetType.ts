408a5c78dc75ab33877f7a936635a637c7c7051a
jsus <esseswann@gmail.com>
2021-11-20 15:58:12 +0300

    Cleanup

--- src/query/extractTargetType.ts
+++ src/query/extractTargetType.ts
@@ -1,6 +1,5 @@
 import { Kind, TypeNode } from 'graphql'
 
-// FIXME move to utils
 function extractTargetType(type: TypeNode): string {
   if (type.kind === Kind.NAMED_TYPE) return type.name.value
   else return extractTargetType(type.type)
